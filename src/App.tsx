c48ddc02fb37ac64588339f1c15614c9d1eae651
Jonas Souza <jonasmzsouza@gmail.com>
2022-07-24 14:36:56 -0300

    :heavy_plus_sign: update(navbar): add navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
-    <h1>Hello GitHub API</h1>
+    <>
+      <Navbar />
+      <h1>Hello GitHub API</h1>
+    </>
   );
 }
 
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-primary main-nav">
+      <a href="/" className="nav-logo-text">
+        <h1>Github API</h1>
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
