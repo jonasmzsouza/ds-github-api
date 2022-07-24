f9bc7798c30da1cfd7bae6ad76686bf6399a4e33
Jonas Souza <jonasmzsouza@gmail.com>
2022-07-24 15:04:20 -0300

    :heavy_plus_sign: update(home): add home styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './assets/styles/custom.scss';
 import './App.css';
 import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1>Hello GitHub API</h1>
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h2>Desafio Github API</h2>
+      <p>Bootcamp Spring React - DevSuperior</p>
+      <a href="/profilesearch">
+        <button className="btn btn-primary btn-lg start-button">Começar</button>
+      </a>
+    </div>
+  );
+};
+
+export default Home;
