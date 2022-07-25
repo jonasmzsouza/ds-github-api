8b66e1f16920d8d75cff8f28e86cf710fa11c35e
Jonas Souza <jonasmzsouza@gmail.com>
2022-07-24 23:36:28 -0300

    :white_check_mark: chore(profilesearch): handle form data

--- src/pages/ProfileSearch/index.tsx
+++ src/pages/ProfileSearch/index.tsx
@@ -1,18 +1,39 @@
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
 import './styles.css';
 
+type FormData = {
+  profile: string;
+};
+
 const ProfileSearch = () => {
+  const [formData, setFormData] = useState<FormData>({
+    profile: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+  }
+
   return (
     <div className="container profile-container">
       <div className="profile-card">
         <h2>Encontre um perfil Github</h2>
-        <form onSubmit={() => {}}>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
               className="search-input"
               placeholder="Usuário Github"
-              onChange={() => {}}
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Encontrar
@@ -21,7 +42,7 @@ const ProfileSearch = () => {
         </form>
       </div>
 
-      <ResultCard />      
+      <ResultCard />
     </div>
   );
 };
